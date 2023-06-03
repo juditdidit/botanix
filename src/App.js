5ad26eeaec3ba8564a8765aa4c63e43d1e6ff2fb
Judit <hello@juditdidit.com>
2023-06-02 18:24:33 -0700

    testing out lists, image imports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+
+const plants = [
+    {
+        name: 'Monty',
+        latin: 'Monstera deliciosa',
+        location: 'Living room floor',
+        sunExposure: '???',
+        water: '???',
+        toxic: {
+            cats: true,
+            dogs: true,
+            kids: true,
+        },
+        photo: 'monstera.jpg',
+    }, {
+        name: 'Bethany',
+        latin: 'Pothos; Epipremmum aureum',
+        location: 'Bookshelf',
+        sunExposure: '???',
+        water: '???',
+        toxic: {
+            cats: true,
+            dogs: true,
+            kids: true,
+        },
+        photo: 'pothos.jpg',
+    }, {
+        name: 'Snek',
+        latin: 'Dracaena trifasciata',
+        location: 'Bedroom',
+        sunExposure: '???',
+        water: '???',
+        toxic: {
+            cats: true,
+            dogs: true,
+            kids: true,
+        },
+        photo: 'snakeplant.jpg',
+    }, {
+        name: 'Arrowhead',
+        latin: 'Syngonium podophyllum',
+        location: 'Bedroom',
+        sunExposure: '???',
+        water: '???',
+        toxic: {
+            cats: true,
+            dogs: true,
+            kids: true,
+        },
+        photo: 'arrowhead.jpg',
+    },
+
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const plantList = plants.map(plant =>
+        <PlantCard plant={plant} />
+    );
+
+    return (
+        <main>
+            <div className="container">
+                <div className="plant-list">
+                    {plantList}
+                </div>
+            </div>
+        </main>
+    );
+}
+
+function PlantCard({plant}) {
+    return (
+        <div className="plant-card">
+            <div className="photo" style={{backgroundImage: `url(../images/${plant.photo})`}}></div>
+            <div className="main">
+                <small className="location">{plant.location}</small>
+                <div className="name">{plant.name}</div>
+                <em className="latin">{plant.latin}</em>
+            </div>
+        </div>
+    );
 }
 
 export default App;

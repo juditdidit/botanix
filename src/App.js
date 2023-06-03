63f95c18a62b37e09085cbaf14086d0cb3051df8
Judit <hello@juditdidit.com>
2023-06-02 20:37:53 -0700

    adding toxicity

--- src/App.js
+++ src/App.js
@@ -2,81 +2,94 @@ import "./App.scss";
 
 const plants = [
     {
-        name: 'Monty',
-        latin: 'Monstera deliciosa',
-        location: 'Living room floor',
-        sunExposure: '???',
-        water: '???',
-        toxic: {
+        id: 1,
+        name: "Monty",
+        latin: "Monstera deliciosa",
+        location: "Living room floor",
+        sunExposure: "???",
+        water: "???",
+        toxicity: {
             cats: true,
             dogs: true,
-            kids: true,
+            kids: false,
         },
-        photo: 'monstera.jpg',
-    }, {
-        name: 'Bethany',
-        latin: 'Pothos; Epipremmum aureum',
-        location: 'Bookshelf',
-        sunExposure: '???',
-        water: '???',
-        toxic: {
-            cats: true,
-            dogs: true,
-            kids: true,
-        },
-        photo: 'pothos.jpg',
-    }, {
-        name: 'Snek',
-        latin: 'Dracaena trifasciata',
-        location: 'Bedroom',
-        sunExposure: '???',
-        water: '???',
-        toxic: {
-            cats: true,
-            dogs: true,
-            kids: true,
-        },
-        photo: 'snakeplant.jpg',
-    }, {
-        name: 'Arrowhead',
-        latin: 'Syngonium podophyllum',
-        location: 'Bedroom',
-        sunExposure: '???',
-        water: '???',
-        toxic: {
-            cats: true,
-            dogs: true,
-            kids: true,
-        },
-        photo: 'arrowhead.jpg',
+        photo: "monstera.jpg",
     },
-
+    {
+        id: 2,
+        name: "Bethany",
+        latin: "Pothos; Epipremmum aureum",
+        location: "Bookshelf",
+        sunExposure: "???",
+        water: "???",
+        toxicity: {
+            cats: true,
+            dogs: true,
+            kids: true,
+        },
+        photo: "pothos.jpg",
+    },
+    {
+        id: 3,
+        name: "Snek",
+        latin: "Dracaena trifasciata",
+        location: "Bedroom",
+        sunExposure: "???",
+        water: "???",
+        toxicity: {
+            cats: true,
+            dogs: true,
+            kids: false,
+        },
+        photo: "snakeplant.jpg",
+    },
+    {
+        id: 4,
+        name: "Arrowhead",
+        latin: "Syngonium podophyllum",
+        location: "Bedroom",
+        sunExposure: "???",
+        water: "???",
+        toxicity: {},
+        photo: "arrowhead.jpg",
+    },
 ];
 
 function App() {
-    const plantList = plants.map(plant =>
-        <PlantCard plant={plant} />
-    );
+    const plantList = plants.map((plant) => <PlantCard plant={plant} key={plant.id} />);
 
     return (
         <main>
             <div className="container">
-                <div className="plant-list">
-                    {plantList}
-                </div>
+                <div className="plant-list">{plantList}</div>
             </div>
         </main>
     );
 }
 
-function PlantCard({plant}) {
+function PlantCard({ plant }) {
+    const toxicityList = Object.keys(plant.toxicity).map(icon => {
+        if (plant.toxicity[icon]) {
+            return <span key={icon}>{icon}</span>
+        } else {
+            return null;
+        }
+    });
+
     return (
         <div className="plant-card">
-            <div className="photo" style={{backgroundImage: `url(../images/${plant.photo})`}}></div>
+            <div
+                className="photo"
+                style={{ backgroundImage: `url(../images/${plant.photo})` }}
+            ></div>
             <div className="main">
                 <small className="location">{plant.location}</small>
                 <div className="name">{plant.name}</div>
                 <em className="latin">{plant.latin}</em>
+
+                <div className="toxicity">
+                    {toxicityList}
+                </div>
             </div>
         </div>
     );
